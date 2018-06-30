54c80bd2d12cb2c4068f8f8d87836722f7324dbc
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2018-06-30 13:29:17 +0100

    feat: populate db

--- app/src/main/java/com/onadasoft/roomwordsample/db/WordRoomDatabase.java
+++ app/src/main/java/com/onadasoft/roomwordsample/db/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.onadasoft.roomwordsample.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import com.onadasoft.roomwordsample.db.dao.WordDao;
 import com.onadasoft.roomwordsample.db.entity.Word;
@@ -34,7 +37,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if(INSTANCE == null){
                     // Create DB
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            WordRoomDatabase.class, "word_database").build();
+                            WordRoomDatabase.class, "word_database").addCallback(sRoomDatabaseCallback).build();
 
                 }
 
@@ -43,6 +46,35 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
     // ---
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+        new RoomDatabase.Callback(){
+
+            @Override
+            public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                super.onOpen(db);
+                new PopulateDbAsync(INSTANCE).execute();
+            }
+        };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            return null;
+        }
+    }
 
 
     // In case of migration --> https://medium.com/google-developers/understanding-migrations-with-room-f01e04b07929
